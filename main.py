9c45c96180600f14ed949b98ed7c9cd4cb59cfc9
koonwen <koonwen@gmail.com>
2021-09-11 20:32:33 +0800

    Add telegram bot functionality.

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-import telegram
-import subprocess
+import os
+import logging
 import time
 
-time.sleep(60)
-bot = telegram.Bot(token="YOUR BOT TOKEN GOES HERE")
+import telegram
+import subprocess
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
+USER_ID = os.environ.get("USER_ID")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Send out ip address
 msg = subprocess.check_output("hostname -I", shell=True).decode('ascii')
-status = bot.send_message(chat_id="@YOUR CHANNEL ID", text=msg, parse_mode=telegram.ParseMode.HTML)
-print(status)
+bot.send_message(chat_id=USER_ID, text=f"I'm up and running at {msg}")
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def ip(update, context):
+    msg = subprocess.check_output("hostname -I", shell=True).decode('ascii')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+
+def temp(update, context):
+    msg = subprocess.check_output("vcgencmd measure_temp", shell=True).decode("ascii")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+start_handler = CommandHandler('start', start)
+ip_handler = CommandHandler('ip', ip)
+temp_handler = CommandHandler('temp', temp)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(ip_handler)
+dispatcher.add_handler(temp_handler)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
+updater.idle()
