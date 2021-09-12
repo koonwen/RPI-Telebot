6b2f12545a067209f6b87cde69162ea828ebefdc
koonwen <koonwen@gmail.com>
2021-09-12 14:52:17 +0800

    Add pic command handler

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 import telegram
 import subprocess
+from picamera import PiCamera
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -34,7 +35,13 @@ def temp(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def pic(update, context):
-    pass
+    with PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture('pic.jpg', resize=(640, 480))
+    context.bot.send_photo(USER_ID, open('~/pic.jpg', 'rb'))
 
 def shutdown(update, context):
     subprocess.run("sudo shutdown now")
@@ -45,12 +52,14 @@ def unknown(update, context):
 start_handler = CommandHandler('start', start)
 ip_handler = CommandHandler('ip', ip)
 temp_handler = CommandHandler('temp', temp)
+pic_handler = CommandHandler('pic', pic)
 shutdown_handler = CommandHandler('shutdown', shutdown)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(ip_handler)
 dispatcher.add_handler(temp_handler)
+dispatcher.add_handler(pic_handler)
 dispatcher.add_handler(shutdown_handler)
 dispatcher.add_handler(unknown_handler)
 
